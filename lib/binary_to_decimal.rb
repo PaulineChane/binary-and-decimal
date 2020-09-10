612cc136acd3a3851214512d3f728f47336d3b75
Pauline Chane <pauline.chane@gmail.com>
2020-09-09 18:50:14 -0700

    Added binary_to_decimal and decimal_to_binary methods to binary_to_decimal.rb, added additional tests for decimal_to_binary method to binary_to_decimal_test.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,3 +1,12 @@
+# Pauline Chane (@PaulineChane on GitHub)
+# Ada Developers Academy C14
+# binary_to_decimal method for CS Fundamentals
+# 09/15/2020
+#
+# binary_to_decimal and decimal_to_binary methods written.
+# Tests for decimal_to_binary have been added into binary_to_decimal_test.rb
+
+
 # A method named `binary_to_decimal` that receives as input an array of size 8.
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
@@ -5,5 +14,25 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_num = 0 # default variable
+
+  # use enum to sum over entire array based off binary -> decimal conversion
+  binary_array.each_with_index {|digit, index| decimal_num += digit * (2 ** (7-index))} # we are indexing backwards!
+
+  return decimal_num
 end
+
+# BONUS: write a method that accepts a decimal integer and converts digit by digit to binary in a array of
+#        single digits.
+
+def decimal_to_binary(decimal_num)
+  binary_array = [] # shovel array
+
+  while decimal_num != 0 do # when original number has been divided over and over to 0, we have reached the end
+    binary_array.prepend(decimal_num % 2) # remainder stored as element in array from right to left
+    decimal_num /= 2 # divide by 2 to determine next remainder to find
+  end
+
+  return binary_array
+end
+
--- test/binary_to_decimal_test.rb
+++ test/binary_to_decimal_test.rb
@@ -1,3 +1,5 @@
+# Edited by @PaulineChane on GitHub to add 3 tests for decimal_to_binary method
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/binary_to_decimal'
@@ -32,4 +34,32 @@ describe "binary to decimal" do
 
     expect(binary_to_decimal(binary_array)).must_equal expected_decimal
   end
+end
+
+# BONUS 3 TESTS FOR decimal_to_binary BONUS METHOD
+
+describe "decimal to binary" do
+  it "From 153 to 10011001" do
+    binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
+    expected_decimal = 153
+
+    expect(decimal_to_binary(expected_decimal)).must_equal binary_array
+  end
+
+  #
+
+  it "From 13 to 00001101" do
+    binary_array = [1, 1, 0, 1]
+    expected_decimal = 13
+
+    expect(decimal_to_binary(expected_decimal)).must_equal binary_array
+  end
+
+  it "From 128 to 10000000" do
+    binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
+    expected_decimal = 128
+
+    expect(decimal_to_binary(expected_decimal)).must_equal binary_array
+  end
+
 end
